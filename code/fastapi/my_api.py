91bbbf3a76b45ee2d47ec4037725bbc6f94950c3
tomo-cps <tomoaki.ohkawa@cps.akita-pu.ac.jp>
2023-05-12 03:02:13 +0900

    change dir

--- code/fastapi/my_api.py
+++ code/fastapi/my_api.py
@@ -4,6 +4,16 @@ app = FastAPI()
 from pydantic import BaseModel
 from typing import List
 
+import sys
+import os
+
+# 現在のディレクトリを取得
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# 親ディレクトリのパスを取得
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+# 親ディレクトリをPythonのパスに追加
+sys.path.append(parent_dir+"/img2text/")
+# モジュールをインポート
 import main
 
 class Preference(BaseModel):
@@ -14,5 +24,6 @@ async def recommend():
     output = main.BooksRecommendation()
     return output.get_recommend()
 
+## pip install fastapi, uvicorn
 ## uvicorn my_api:app --port 8080   
 ## curl -X POST -H "Content-Type: application/json" http://localhost:8080/recommend
